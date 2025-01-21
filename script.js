0f0edb6601fa1456fa04cd9a55d4b25ad0cd8296
Prajwal Markal Puttaswamy <85050151+prajwal467262@users.noreply.github.com>
2025-01-21 17:20:27 +0000

    Portfolio Finished

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// Function to toggle menu and handle sticky navigation
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  
-    // Handle sticky navigation
-    const nav = document.getElementById('desktop-nav');
-    if (window.scrollY > 0) {
-      nav.classList.add('sticky');
-    } else {
-      nav.classList.remove('sticky');
-    }
-  }
-  
-  // Event listener for menu toggle
-  document.querySelector(".hamburger-icon").addEventListener("click", toggleMenu);
-  
-  // Event listener for scroll to handle sticky navigation
-  window.addEventListener('scroll', function() {
-    var nav = document.getElementById('desktop-nav');
-    if (window.scrollY > 0) {
-        nav.classList.add('sticky');
-    } else {
-        nav.classList.remove('sticky');
-    }
-});
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
   
